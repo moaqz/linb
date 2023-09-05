086596feecc57eb381419064caf80b7bd17d1129
moaqz <goz.matu@gmail.com>
2023-09-05 12:13:40 -0300

    perf: add indexes to database schemas

--- src/server/schema.ts
+++ src/server/schema.ts
@@ -5,27 +5,47 @@ import {
   text,
   integer,
   timestamp,
+  index,
 } from "drizzle-orm/pg-core";
 
 export const visibilityEnum = pgEnum("visibility", ["public", "private"]);
 
-export const collections = pgTable("collections", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  visibility: visibilityEnum("visibility").notNull().default("private"),
-  user_id: text("user_id").notNull(),
-  created_at: timestamp("created_at").notNull().defaultNow(),
-  updated_at: timestamp("updated_at").notNull().defaultNow(),
-});
+export const collections = pgTable(
+  "collections",
+  {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    visibility: visibilityEnum("visibility").notNull().default("private"),
+    user_id: text("user_id").notNull(),
+    created_at: timestamp("created_at").notNull().defaultNow(),
+    updated_at: timestamp("updated_at").notNull().defaultNow(),
+  },
+  (table) => {
+    return {
+      userIdx: index("user_id_idx").on(table.user_id),
+    };
+  },
+);
 
-export const links = pgTable("links", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  url: text("url").notNull(),
-  collection_id: integer("collection_id")
-    .notNull()
-    .references(() => collections.id),
-  user_id: text("user_id").notNull(),
-  created_at: timestamp("created_at").notNull().defaultNow(),
-  updated_at: timestamp("updated_at").notNull().defaultNow(),
-});
+export const links = pgTable(
+  "links",
+  {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    url: text("url").notNull(),
+    collection_id: integer("collection_id")
+      .notNull()
+      .references(() => collections.id),
+    user_id: text("user_id").notNull(),
+    created_at: timestamp("created_at").notNull().defaultNow(),
+    updated_at: timestamp("updated_at").notNull().defaultNow(),
+  },
+  (table) => {
+    return {
+      userCollectionIdx: index("user_collection_idx").on(
+        table.user_id,
+        table.collection_id,
+      ),
+    };
+  },
+);
