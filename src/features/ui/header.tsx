ccb5adee2c0721d505afcf721711ae9b288dc223
moaqz <goz.matu@gmail.com>
2023-09-08 11:41:16 -0300

    feat: display user name

--- src/features/ui/header.tsx
+++ src/features/ui/header.tsx
@@ -1,12 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
-import { Logo } from "./logo";
+import { Logo } from ".";
 
 export function Header() {
   return (
     <header className="mb-6 border-b-2 border-b-black bg-white">
       <div className="max-w-screen-lg mx-auto flex items-center justify-between p-4">
         <Logo />
-        <UserButton />
+        <UserButton showName />
       </div>
     </header>
   );
