c12f66f61c8de0ab193f1b06bc6ab6389bbc0e78
moaqz <goz.matu@gmail.com>
2023-09-21 17:17:28 -0300

    feat(links): rewrite services using actions

---
+++ src/features/links/actions/create-link-action.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { AuthRequiredError } from "@/lib/expection";
+import { currentUser } from "@clerk/nextjs";
+import { parse } from "valibot";
+import { CreateLinkSchema } from "../validations";
+import { links } from "@/server/schema";
+import { db } from "@/server/connection";
+import { revalidatePath } from "next/cache";
+
+export const createLinkAction = async (data: FormData) => {
+  const user = await currentUser();
+  if (user == null) {
+    throw new AuthRequiredError();
+  }
+
+  try {
+    const { collectionId, name, url } = parse(CreateLinkSchema, {
+      collectionId: Number(data.get("collectionId")),
+      name: data.get("name"),
+      url: data.get("url"),
+    });
+
+    await db.insert(links).values({
+      name,
+      url,
+      user_id: user.id,
+      collection_id: collectionId,
+    });
+
+    revalidatePath(`/collections/${collectionId}`);
+    return { message: "Success" };
+  } catch (error) {
+    console.log(error);
+    return {
+      error: "Something went wrong!"
+    };
+  }
+};
---
+++ src/features/links/actions/delete-link-action.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { AuthRequiredError } from "@/lib/expection";
+import { db } from "@/server/connection";
+import { links } from "@/server/schema";
+import { currentUser } from "@clerk/nextjs";
+import { and, eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+
+export const deleteLinkAction = async (data: FormData) => {
+  const user = await currentUser();
+  if (user == null) {
+    throw new AuthRequiredError();
+  }
+
+  const linkId = Number(data.get("link_id"));
+  const collectionId = data.get("collection_id");
+
+  if (isNaN(linkId) || linkId <= 0) {
+    return {
+      error: "invalid link id"
+    };
+  }
+
+  if (!collectionId || typeof collectionId !== "string") {
+    return {
+      error: "invalid collection id"
+    };
+  }
+
+  try {
+    await db
+      .delete(links)
+      .where(and(eq(links.user_id, user.id), eq(links.id, linkId)));
+
+    revalidatePath(`/collections/${collectionId}`);
+    return { message: "Link successfully deleted!" };
+  } catch (error) {
+    return { message: "There was an error." };
+  }
+};
