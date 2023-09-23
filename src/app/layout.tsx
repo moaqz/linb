736ec47e20b39ca0e4af093effdd3e978d7e3994
moaqz <goz.matu@gmail.com>
2023-09-23 10:48:54 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,14 @@ export const metadata: Metadata = {
   title: "Linb - Find your Favorite websites in seconds",
   description:
     "A Simple and Intuitive Bookmarking Application to Organize Your Links.",
+  themeColor: "#E3FD62",
+  openGraph: {
+    siteName: "Linb - Find your Favorite websites in seconds",
+    description:
+      "A Simple and Intuitive Bookmarking Application to Organize Your Links.",
+    url: "https://linb.vercel.app",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
