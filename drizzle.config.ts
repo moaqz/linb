c892ca471193d6ad546e2e9090acbc86c9020f26
moaqz <goz.matu@gmail.com>
2023-12-26 15:05:04 -0300

    fix: files ignored by biome

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from "drizzle-kit";
 
-export default {
+export default ({
   schema: "./src/server/schema.ts",
   driver: "pg",
   out: "./src/server/migrations",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL ?? "",
   },
-} satisfies Config;
+} satisfies Config);
